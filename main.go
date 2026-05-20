76f4c2f4f0c95a4d63ff607ce0d7ef7ebcb85bf2
himanshi.gupta@562 <himanshi.gupta@ratnaglobaltech.com>
2026-05-20 13:03:48 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	result := Add(10, 5)
+
+	fmt.Fprintf(
+		w,
+		"Go Jenkins Demo App - Running!<br>10 + 5 = %d",
+		result,
+	)
+}
 
 func main() {
-    fmt.Println("Go Jenkins Demo App - Running!")
-    result := Add(10, 5)
-    fmt.Printf("10 + 5 = %d\n", result)
+	http.HandleFunc("/", homeHandler)
+
+	fmt.Println("Server running on http://localhost:8085")
+
+	err := http.ListenAndServe(":8085", nil)
+
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
